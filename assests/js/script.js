ba52f934b2867c3830eeaf0ca5ed87aea4ed3536
Koi-AS <lightningblade.901@gmail.com>
2023-04-14 16:26:06 -0400

    Added a feature for when a user submits an city name, a new button is created below the search bar

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,15 +1,57 @@
 // Beginning of JS file
 
-//My OpenWeather API Key
+// Pseudocode
+// 1 When I search for a city name
+//      User types in input field
+//      Save user data
+//      Create a button to find that city again (local storage?)
+//      Activate weather data
+// 2 then I can see todays date and the weather data for that city
+//      For the main card
+//          Use dayjs to display the date for today
+                // var today = dayjs().format('M/D/YYYY');
+                // var mainDateEl = $('#todayDate')
+                // mainDateEl.text(today);
+//          Use openweatherapi to display the weather conditions, temperature, wind speeds, and humidity
+//      For the forecast cards
+//          use dayjs to display the next 5 days dates
+//          user openweather api to display the weather conditions, temperature, wind speeds, and humidity
+
+// openweather things to use:
+//      list.main.temp      | calls temperature, default kelvin -> change to imperial fahrenheit
+//      list.main.humidity  | calls humidity %
+//      list.wind.speed     | calls wind speed, default m/s -> change to imperial mph
+//      forecast.symbol.var | gets weather icon id
+
+// My OpenWeather API Key
 var weatherKey = '1f8dd424d44ab8bddae9917decbf64a3';
-var city;
+var city = [];
 
 var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey;
 
+console.log('hi')
 fetch(queryUrl)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-
+        console.log(data);
     });
+
+var inputEl = $('#inputSearch')
+var submitEl = $('#submitSearch')
+var saveBtnEl = $('#citySaveBar')
+
+submitEl.on('click', function() {
+    if(inputEl === '') {
+        alert('Please Choose a Valid City Name!')
+        return;
+    }
+    // Sets the value of 'city' to the user input
+    city = inputEl.val();
+    // These lines create, style, display, and append a button for each new user input
+    var cityBtn = $('<button>')
+    cityBtn.addClass("col-12 p-1 my-2 text-dark bg-gray border-0 rounded");
+    cityBtn.text(city)
+    saveBtnEl.append(cityBtn)
+})
