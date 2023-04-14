f495bede81824381179222fb7d577c911fe701f4
Koi-AS <lightningblade.901@gmail.com>
2023-04-14 17:43:57 -0400

    Display for the main card after user input works

--- assests/js/script.js
+++ assests/js/script.js
@@ -25,26 +25,19 @@
 
 // My OpenWeather API Key
 var weatherKey = '1f8dd424d44ab8bddae9917decbf64a3';
-var city = [];
-
-// console.log('hi')
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-//         console.log(data.coord.lat)
-//         console.log(data.coord.lon)
-//     });
-
+var city;
+// Input buttons for user to input city names
 var inputEl = $('#inputSearch')
 var submitEl = $('#submitSearch')
-var saveBtnEl = $('#citySaveBar')
+var saveBtnEl = $('#citySaveBar') // these are the buttons that will hold saved city names
+// Main card display elements
+var mainCity = $('#cityName')
+var mainTemp = $('#tempMain')
+var mainWind = $('#windSpeedMain')
+var mainHumidity = $('#humidityMain')
 
-submitEl.on('click', function() {
+submitEl.on('click', function() { // when a user clicks the submit button
     inputEl.val().trim();
-    console.log(inputEl.val().trim())
     if(inputEl.val() === '') {
         alert('Please Choose a Valid City Name!')
         return;
@@ -52,23 +45,29 @@ submitEl.on('click', function() {
     // Sets the value of 'city' to the user input
     city = inputEl.val();
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey)
-    .then(function(response) {
-        if(response.status !== 200) {
-            console.log(response.status)
-            return;
-        }
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        console.log(data.coord.lat)
-        console.log(data.coord.lon)
-
-        // These lines create, style, display, and append a button for each new user input
-        var cityBtn = $('<button>')
-        cityBtn.addClass("col-12 p-1 my-2 text-dark bg-gray border-0 rounded");
-        cityBtn.text(city)
-        saveBtnEl.append(cityBtn)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey + '&units=imperial') // then the OpenWeatherAPI is called
+        .then(function(response) {
+            if(response.status !== 200) { // If the city isn't found
+                console.log(response.status) // log an error message
+                return; // and stop the code
+            }
+            return response.json(); // else run the code as normal
+        })
+        .then(function(data) { //using the data
+            console.log(data);
+            // Sets weather data to reusable variable
+            var tempData = data.main.temp;
+            var windData = data.wind.speed;
+            var humidityData = data.main.humidity;
+            // Displays the weather data to the main card
+            mainCity.text(city);
+            mainTemp.text('Temperature: ' + tempData + 'F');
+            mainWind.text('Wind Speed: ' + windData + 'MpH');
+            mainHumidity.text('Humidity: ' + humidityData + '%');
+            // These lines create, style, display, and append a button for each new user input
+            var cityBtn = $('<button>')
+            cityBtn.addClass("col-12 p-1 my-2 text-dark bg-gray border-0 rounded");
+            cityBtn.text(city)
+            saveBtnEl.append(cityBtn)
     });
 })
