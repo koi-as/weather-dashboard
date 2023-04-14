7d49788cd661366d6f5dd09b8900c4f569cb0f5e
Koi-AS <lightningblade.901@gmail.com>
2023-04-14 17:08:01 -0400

    The search bar will now filter out results that are not city names

--- assests/js/script.js
+++ assests/js/script.js
@@ -27,31 +27,48 @@
 var weatherKey = '1f8dd424d44ab8bddae9917decbf64a3';
 var city = [];
 
-var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey;
-
-console.log('hi')
-fetch(queryUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+// console.log('hi')
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//         console.log(data.coord.lat)
+//         console.log(data.coord.lon)
+//     });
 
 var inputEl = $('#inputSearch')
 var submitEl = $('#submitSearch')
 var saveBtnEl = $('#citySaveBar')
 
 submitEl.on('click', function() {
-    if(inputEl === '') {
+    inputEl.val().trim();
+    console.log(inputEl.val().trim())
+    if(inputEl.val() === '') {
         alert('Please Choose a Valid City Name!')
         return;
     }
     // Sets the value of 'city' to the user input
     city = inputEl.val();
-    // These lines create, style, display, and append a button for each new user input
-    var cityBtn = $('<button>')
-    cityBtn.addClass("col-12 p-1 my-2 text-dark bg-gray border-0 rounded");
-    cityBtn.text(city)
-    saveBtnEl.append(cityBtn)
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey)
+    .then(function(response) {
+        if(response.status !== 200) {
+            console.log(response.status)
+            return;
+        }
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.coord.lat)
+        console.log(data.coord.lon)
+
+        // These lines create, style, display, and append a button for each new user input
+        var cityBtn = $('<button>')
+        cityBtn.addClass("col-12 p-1 my-2 text-dark bg-gray border-0 rounded");
+        cityBtn.text(city)
+        saveBtnEl.append(cityBtn)
+    });
 })
