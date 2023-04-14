b86ed8d4472481afbc2c908b5d7d1ea5cab0b4a9
Koi-AS <lightningblade.901@gmail.com>
2023-04-13 22:10:43 -0400

    Added my OpenWeather API key to the script.js file

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,15 @@
 // Beginning of JS file
+
+//My OpenWeather API Key
+var weatherKey = '1f8dd424d44ab8bddae9917decbf64a3';
+var city;
+
+var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey;
+
+fetch(queryUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+
+    });
