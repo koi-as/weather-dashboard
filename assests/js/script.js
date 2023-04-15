b43b901542c8a32ec94cc676a0cf356d0c244cb3
Koi-AS <lightningblade.901@gmail.com>
2023-04-15 16:13:30 -0400

    Display for the temperature, wind speed, and humidity appear on all of the cards. Weather icon placeholders have been displayed for each card.

--- assests/js/script.js
+++ assests/js/script.js
@@ -9,9 +9,9 @@
 // 2 then I can see todays date and the weather data for that city
 //      For the main card
 //          Use dayjs to display the date for today
-                // var today = dayjs().format('M/D/YYYY');
-                // var mainDateEl = $('#todayDate')
-                // mainDateEl.text(today);
+// var today = dayjs().format('M/D/YYYY');
+// var mainDateEl = $('#todayDate')
+// mainDateEl.text(today);
 //          Use openweatherapi to display the weather conditions, temperature, wind speeds, and humidity
 //      For the forecast cards
 //          use dayjs to display the next 5 days dates
@@ -30,44 +30,95 @@ var city;
 var inputEl = $('#inputSearch')
 var submitEl = $('#submitSearch')
 var saveBarEl = $('#citySaveBar')
+// An empty array to save city names to
+var cityArr = [];
 // A button element class to use in a click event for when any button is pressed
 var btnEl = $('.button')
-// Main card display elements
-var mainCity = $('#cityName')
-var mainWeather = $('#weatherIcon')
-var mainTemp = $('#tempMain')
-var mainWind = $('#windSpeedMain')
-var mainHumidity = $('#humidityMain')
-// Shows todays date
-var today = dayjs().format('M/D/YYYY');
-var mainDateEl = $('#todayDate')
-mainDateEl.text(today);
 
 function displayData(city) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherKey + '&units=imperial') // then the OpenWeatherAPI is called
-        .then(function(response) {
-            if(response.status !== 200) { // If the city isn't found
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + weatherKey + '&units=imperial')
+        .then(function (response) {
+            if (response.status !== 200) { // If the city isn't found
                 console.log(response.status) // log an error message
                 alert('Please Choose a Valid City Name!') // alert the user about the issue
                 return; // and stop the code
             }
             return response.json(); // else run the code as normal
         })
-        .then(function(data) { //using the data
-            console.log(data);
-            // Sets weather data to reusable variable
-            var tempData = data.main.temp;
-            var windData = data.wind.speed;
-            var humidityData = data.main.humidity;
+        .then(function (data) {
+            console.log(data)
+            // date display for weather data cards
+            // pulls unix timestamp for each date
+            var today = data.list[0].dt;
+            var nextDay1 = data.list[7].dt;
+            var nextDay2 = data.list[15].dt;
+            var nextDay3 = data.list[23].dt;
+            var nextDay4 = data.list[31].dt;
+            var nextDay5 = data.list[39].dt;
+            // converts unix timestamp to readable format
+            var todayDisplay = dayjs.unix(today).format('M/D/YYYY');
+            var nextDay1Display = dayjs.unix(nextDay1).format('M/D/YYYY');
+            var nextDay2Display = dayjs.unix(nextDay2).format('M/D/YYYY');
+            var nextDay3Display = dayjs.unix(nextDay3).format('M/D/YYYY');
+            var nextDay4Display = dayjs.unix(nextDay4).format('M/D/YYYY');
+            var nextDay5Display = dayjs.unix(nextDay5).format('M/D/YYYY');
+            // displays readable date to each fo the cards
+            $('#todayDate').text(todayDisplay);
+            $('#card1 #cardDate').text(nextDay1Display);
+            $('#card2 #cardDate').text(nextDay2Display);
+            $('#card3 #cardDate').text(nextDay3Display);
+            $('#card4 #cardDate').text(nextDay4Display);
+            $('#card5 #cardDate').text(nextDay5Display);
+
+            // temperature, wind speed, and humidity display for weather cards
+            // temperature display
+            var todayTemp = data.list[0].main.temp;
+            var nextTemp1 = data.list[7].main.temp;
+            var nextTemp2 = data.list[15].main.temp;
+            var nextTemp3 = data.list[23].main.temp;
+            var nextTemp4 = data.list[31].main.temp;
+            var nextTemp5 = data.list[39].main.temp;
+
+            $('#tempMain').text('Temperature: ' + todayTemp + 'F');
+            $('#card1 #cardTemp').text('Temperature: ' + nextTemp1 + 'F');
+            $('#card2 #cardTemp').text('Temperature: ' + nextTemp2 + 'F');
+            $('#card3 #cardTemp').text('Temperature: ' + nextTemp3 + 'F');
+            $('#card4 #cardTemp').text('Temperature: ' + nextTemp4 + 'F');
+            $('#card5 #cardTemp').text('Temperature: ' + nextTemp5 + 'F');
+            // wind speed display
+            var todayWind = data.list[0].wind.speed;
+            var nextWind1 = data.list[7].wind.speed;
+            var nextWind2 = data.list[15].wind.speed;
+            var nextWind3 = data.list[23].wind.speed;
+            var nextWind4 = data.list[31].wind.speed;
+            var nextWind5 = data.list[39].wind.speed;
+
+            $('#windSpeedMain').text('Wind Speed: ' + todayWind + ' MpH');
+            $('#card1 #cardWind').text('Wind Speed: ' + nextWind1 + ' MpH');
+            $('#card2 #cardWind').text('Wind Speed: ' + nextWind2 + ' MpH');
+            $('#card3 #cardWind').text('Wind Speed: ' + nextWind3 + ' MpH');
+            $('#card4 #cardWind').text('Wind Speed: ' + nextWind4 + ' MpH');
+            $('#card5 #cardWind').text('Wind Speed: ' + nextWind5 + ' MpH');
+            // humidity display
+            var todayHumid = data.list[0].main.humidity;
+            var nextHumid1 = data.list[7].main.humidity;
+            var nextHumid2 = data.list[15].main.humidity;
+            var nextHumid3 = data.list[23].main.humidity;
+            var nextHumid4 = data.list[31].main.humidity;
+            var nextHumid5 = data.list[39].main.humidity;
+
+            $('#humidityMain').text('Humidity: ' + todayHumid + '%');
+            $('#card1 #cardHumidity').text('Humidity: ' + nextHumid1 + '%');
+            $('#card2 #cardHumidity').text('Humidity: ' + nextHumid2 + '%');
+            $('#card3 #cardHumidity').text('Humidity: ' + nextHumid3 + '%');
+            $('#card4 #cardHumidity').text('Humidity: ' + nextHumid4 + '%');
+            $('#card5 #cardHumidity').text('Humidity: ' + nextHumid5 + '%');
             // Sets weather icon
             var iconCode = data.weather[0].icon;
             var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
             // Displays the weather data to the main card
             mainCity.text(city);
             mainWeather.attr('src', iconUrl)
-            mainTemp.text('Temperature: ' + tempData + 'F');
-            mainWind.text('Wind Speed: ' + windData + ' MpH');
-            mainHumidity.text('Humidity: ' + humidityData + '%');
             // These lines create, style, display, and append a button for each new user input
             var cityBtn = $('<button>');
             cityBtn.addClass("button col-12 p-1 my-2 text-dark bg-gray border-0 rounded");
@@ -76,25 +127,11 @@ function displayData(city) {
             saveButtonData(cityBtn);
             // Clear the input area
             inputEl.val('');
-    });
-
-    // fetch('api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + weatherKey)//this will be the forecast api
-    //     .then(function(response) {
-    //         if(response.status !== 200) { // If the city isn't found
-    //             return; // stop the code
-    //         }
-    //         return response.json();
-    //     })
-    //     .then(function(data) {
-    //         console.log(data)
-    //         // call forecast data
-    //         // set data to text of card items
-    //     });
+        });
 };
 
 function saveButtonData(saveBtn) {
     // to save a button
-    var cityArr = [];
     cityArr.push(city)
     console.log(cityArr)
     // need to save the city name to an array
@@ -102,9 +139,9 @@ function saveButtonData(saveBtn) {
     // link the button click to displayData function
 };
 
-submitEl.on('click', function() { // when a user clicks the submit button... cont on line 46
+submitEl.on('click', function () { // when a user clicks the submit button... cont on line 46
     inputEl.val().trim();
-    if(inputEl.val() === '') {
+    if (inputEl.val() === '') {
         alert('Please Choose a Valid City Name!')
         return;
     }
@@ -114,7 +151,7 @@ submitEl.on('click', function() { // when a user clicks the submit button... con
     displayData(city)
 })
 
-btnEl.on('click', function() { // when a user clicks a city name button
+btnEl.on('click', function () { // when a user clicks a city name button
     // call a variable from local storage
     // call the display data function with the variable from ls
 })
